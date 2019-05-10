9427357ae34f4234ae4c17dfbff33410334612b6
Lorne <lorne.luo@butterfly.com.au>
2019-05-10 15:29:35 +1000

    sync with online edition

--- lambda/download_asx_list.py
+++ lambda/download_asx_list.py
@@ -1,10 +1,29 @@
 import boto3
+import json
 import urllib
 
 s3 = boto3.client('s3')
 
 
-def handler(event, context):
+def respond(err, res=""):
+    body = res
+    return {
+        'statusCode': '400' if err else '200',
+        'body': json.dumps({'details': str(err)}) if err else json.dumps(res),
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
+
+
+def lambda_handler(event, context):
+    '''
+    download asx csv to S3
+    '''
+    method = event['httpMethod'].upper()
+    if method not in ['POST']:
+        return respond(ValueError('Unsupported method "{}"'.format(method)))
+
     bucket = 'lorne'
     asx_url = 'https://www.asx.com.au/asx/research/ASXListedCompanies.csv'
     local_path = '/tmp/asx.csv'
@@ -13,43 +32,4 @@ def handler(event, context):
 
     s3.upload_file(local_path, bucket, key)
 
-
-
-'''
-
-import boto3
-import json
-
-print('Loading function')
-dynamo = boto3.client('dynamodb')
-
-
-def respond(err, res=None):
-    return {
-        'statusCode': '400' if err else '200',
-        'body': err.message if err else json.dumps(res),
-        'headers': {
-            'Content-Type': 'application/json',
-        },
-    }
-
-
-def lambda_handler(event, context):
-    
-    #print("Received event: " + json.dumps(event, indent=2))
-
-    operations = {
-        'DELETE': lambda dynamo, x: dynamo.delete_item(**x),
-        'GET': lambda dynamo, x: dynamo.scan(**x),
-        'POST': lambda dynamo, x: dynamo.put_item(**x),
-        'PUT': lambda dynamo, x: dynamo.update_item(**x),
-    }
-
-    operation = event['httpMethod']
-    if operation in operations:
-        payload = event['queryStringParameters'] if operation == 'GET' else json.loads(event['body'])
-        return respond(None, operations[operation](dynamo, payload))
-    else:
-        return respond(ValueError('Unsupported method "{}"'.format(operation)))
-
-'''
+    return respond(None, {'success': True, 'details': None})
