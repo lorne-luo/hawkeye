efb2e167a0305978250421de6cd93df1c3f82324
Lorne <lorne.luo@butterfly.com.au>
2019-05-10 15:29:48 +1000

    add kinesis helper

--- aws.py
+++ aws.py
@@ -1,3 +1,7 @@
+import base64
+import json
+from random import random
+
 import boto3
 import settings
 
@@ -12,6 +16,10 @@ s3 = boto3.client('s3',
                   region_name=settings.AWS_REGION,
                   aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
                   aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
+
+kinesis = boto3.client('kinesis', region_name=settings.AWS_REGION,
+                       aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+                       aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
 
 
 def dynamodb_batch_push(table, items):
@@ -30,3 +38,10 @@ def dynamodb_batch_push(table, items):
         })
         i += BATCH_LIMIT
         print(f'Table {table} request #{int(i/BATCH_LIMIT)}')
+
+
+def kinesis_put_records(stream_name, items):
+    get_partition_key = lambda x: x.get('PartitionKey') or str(random())
+    records = [{'Data': bytes(json.dumps(item), 'utf-8'), 'PartitionKey': get_partition_key(item)} for item in items]
+    print(base64.b64encode(bytes(json.dumps(items[0]), 'utf-8')))
+    kinesis.put_records(StreamName=stream_name, Records=records)
