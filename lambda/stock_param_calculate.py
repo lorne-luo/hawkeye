26eb300663b94c5f9718aa6081f1d91d2dc09aae
Lorne <dev@luotao.net>
2019-05-11 11:03:01 +1000

    test for param calculation

--- lambda/stock_param_calculate.py
+++ lambda/stock_param_calculate.py
@@ -1,15 +1,16 @@
-import os
-import json
-import urllib
-from urllib.parse import unquote_plus
-
 import boto3
-import pandas as pd
+import io
+import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
+import os
+import pandas as pd
+from urllib.parse import unquote_plus
 
 s3 = boto3.client('s3')
 dynamodb = boto3.resource('dynamodb')
 company_table = dynamodb.Table('company')
+bucket = 'lorne'
 
 
 def stock_monte_carlo(start_price, days, mu, sigma):
@@ -44,7 +45,6 @@ def monte_carlo_simulations(start_price, days, mu, sigma, runs=10000):
 
 def test():
     key = 'hawkeye/recent_price/MOQ.csv'
-    bucket = 'lorne'
     csv_file = s3.get_object(Bucket=bucket, Key=key)
 
     df = pd.read_csv(csv_file['Body'], header=0, index_col='timestamp')
@@ -54,21 +54,49 @@ def test():
     return_mean = df['return'].mean()
     return_sigma = df['return'].std()
     start_price = df['close'][-1]
-    print(start_price, volume_mean,return_mean,return_sigma)
+    print(start_price, volume_mean, return_mean, return_sigma)
 
-    # simulations = monte_carlo_simulations(start_price, 30, return_mean, return_sigma)
-    #
-    # percent99 = np.percentile(simulations, 1)
-    # percent90 = np.percentile(simulations, 10)
-    # percent80 = np.percentile(simulations, 20)
-    # percent70 = np.percentile(simulations, 30)
-    # percent60 = np.percentile(simulations, 40)
-    #
-    # sim_mean = simulations.mean()
-    # var = start_price - percent99
-    #
-    # print(percent99, percent90, percent80, percent70, percent60)
-    # print(sim_mean, var)
+    days = 30
+    simulations = monte_carlo_simulations(start_price, days, return_mean, return_sigma)
+
+    percent99 = np.percentile(simulations, 1)
+    percent90 = np.percentile(simulations, 10)
+    percent80 = np.percentile(simulations, 20)
+    percent70 = np.percentile(simulations, 30)
+    percent60 = np.percentile(simulations, 40)
+
+    sim_mean = simulations.mean()
+    var = start_price - percent99
+
+    print(percent99, percent90, percent80, percent70, percent60)
+    print(sim_mean, var)
+
+    plt.figure(figsize=(16, 6))
+    # define q as the 1% empirical qunatile, this basically means that 99% of the values should fall between here
+
+    # plot the distribution of the end prices
+    plt.hist(simulations, bins=200)
+
+    # Using plt.figtext to fill in some additional information onto the plot
+
+    # Starting Price
+    plt.figtext(0.6, 0.8, s="Start price: $%.2f" % start_price)
+    # Mean ending price
+    plt.figtext(0.6, 0.7, "Mean final price: $%.2f" % simulations.mean())
+    plt.figtext(0.6, 0.6, "VaR(0.99): $%.2f" % (start_price - percent99,))
+    plt.figtext(0.15, 0.6, "q(0.99): $%.2f" % percent99)
+    plt.axvline(x=percent99, linewidth=1, color='r')
+    plt.axvline(x=percent90, linewidth=1, color='r')
+    plt.axvline(x=percent80, linewidth=1, color='r')
+    plt.axvline(x=percent70, linewidth=1, color='r')
+    plt.axvline(x=percent60, linewidth=1, color='r')
+    plt.title(u"Final price distribution for Woolworths Stock after %s days" % days, weight='bold')
+
+    # save to S3
+    img_data = io.BytesIO()
+    plt.savefig(img_data, format='png')
+    img_data.seek(0)
+    s3.put_object(Body=img_data, ContentType='image/png', Bucket=bucket, Key='hawkeye/test.png')
 
 
 def lambda_handler(event, context):
@@ -78,7 +106,6 @@ def lambda_handler(event, context):
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
     csv_file = s3.get_object(Bucket=bucket, Key=key)
-
 
     path, filename = os.path.split(key)
     code, _ = os.path.splitext(filename)
@@ -110,7 +137,7 @@ if __name__ == '__main__':
     import time
 
     now = time.time()
-    df = pd.read_csv('/tmp/GSW.csv', header=0, index_col='timestamp')
+    df = pd.read_csv('/tmp/MOQ.csv', header=0, index_col='timestamp')
     df['return'] = df['adjusted_close'].pct_change(1)
     df = df.dropna()
 
@@ -118,7 +145,8 @@ if __name__ == '__main__':
     return_mean = df['return'].mean()
     return_sigma = df['return'].std()
     start_price = df['close'][-1]
-    simulations = monte_carlo_simulations(start_price, 30, return_mean, return_sigma)
+    days = 30
+    simulations = monte_carlo_simulations(start_price, days, return_mean, return_sigma, runs=10000)
 
     percent99 = np.percentile(simulations, 1)
     percent90 = np.percentile(simulations, 10)
@@ -132,3 +160,30 @@ if __name__ == '__main__':
     print(time.time() - now)
     print(percent99, percent90, percent80, percent70, percent60)
     print(sim_mean, var)
+
+    plt.figure(figsize=(16, 6))
+    # define q as the 1% empirical qunatile, this basically means that 99% of the values should fall between here
+
+    # plot the distribution of the end prices
+    plt.hist(simulations, bins=200)
+
+    # Using plt.figtext to fill in some additional information onto the plot
+
+    # Starting Price
+    plt.figtext(0.6, 0.8, s="Start price: $%.2f" % start_price)
+    # Mean ending price
+    plt.figtext(0.6, 0.7, "Mean final price: $%.2f" % simulations.mean())
+    plt.figtext(0.6, 0.6, "VaR(0.99): $%.2f" % (start_price - percent99,))
+    plt.figtext(0.15, 0.6, "q(0.99): $%.2f" % percent99)
+    plt.axvline(x=percent99, linewidth=1, color='r')
+    plt.axvline(x=percent90, linewidth=1, color='r')
+    plt.axvline(x=percent80, linewidth=1, color='r')
+    plt.axvline(x=percent70, linewidth=1, color='r')
+    plt.axvline(x=percent60, linewidth=1, color='r')
+    plt.title(u"Final price distribution for Woolworths Stock after %s days" % days, weight='bold')
+
+    # save to S3
+    img_data = io.BytesIO()
+    plt.savefig(img_data, format='png')
+    img_data.seek(0)
+    s3.put_object(Body=img_data, ContentType='image/png', Bucket=bucket, Key='hawkeye/test.png')
