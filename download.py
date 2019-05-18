f963aa3c6b994d3beae7b2a53f0770366c70dd02
Lorne <dev@luotao.net>
2019-05-18 13:09:36 +1000

    update download

--- download.py
+++ download.py
@@ -1,11 +1,13 @@
-import os
 import time
+
+import os
+import pandas as pd
 from alpha_vantage.timeseries import TimeSeries
 
+import settings
 from asx import get_asx_df
-import pandas as pd
 
-ts = TimeSeries(key=os.environ.get('ALPHA_VANTAGE_API_KEY'), output_format='pandas', indexing_type='date', retries=3)
+ts = TimeSeries(key=settings.ALPHA_VANTAGE_API_KEY, output_format='pandas', indexing_type='date', retries=3)
 
 result_path = './pic/'
 
@@ -26,15 +28,17 @@ if __name__ == '__main__':
     for i in range(len(df)):
         code = df.iloc[i]['ASX code']
         name = df.iloc[i]['Company name']
-        if i < 1073:
-            continue
+        path = f'./price/{code}.csv'
+
+        # if os.path.exists(path):
+        #     continue
 
         try:
             download_csv(code, True)
         except Exception as ex:
             print(f'{i}. {code} raise error: {ex}')
-            time.sleep(31)
+            time.sleep(21)
             continue
 
         print(i, code)
-        time.sleep(31)
+        time.sleep(21)
