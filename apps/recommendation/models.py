0f9f14254c277587653bd01e0bfa9eaf064e18b6
Lorne <dev@luotao.net>
2019-06-19 21:57:43 +1000

    add strategy in recommendation

--- apps/recommendation/models.py
+++ apps/recommendation/models.py
@@ -8,13 +8,15 @@ from apps.prediction.models import WeeklyPrediction
 
 
 class WeeklyRecommendationManager(Manager):
-    def week(self, week):
-        return super().get_queryset().filter(week=week)
+    def week(self, week, strategy):
+        return super().get_queryset().filter(week=week, strategy=strategy)
 
 
 class WeeklyRecommendation(models.Model):
+    strategy = models.CharField('strategy', max_length=80, blank=True, null=False)
     week = models.IntegerField('week', blank=False, null=False)
     code = models.ForeignKey(WeeklyPrediction, blank=True, null=True, on_delete=models.SET_NULL)
+    rank = models.DecimalField('rank', max_digits=10, decimal_places=4, blank=True, null=True)
     future_week_price = models.DecimalField('future week price', max_digits=10, decimal_places=4, blank=True, null=True)
     future_week_return = models.DecimalField('future week return', max_digits=10, decimal_places=4, blank=True,
                                              null=True)
