3ce6eb8df1c58184fe4e2128c5afa944816c7997
Lorne <dev@luotao.net>
2019-06-23 14:13:00 +1000

    sync cmd for company

---
+++ apps/asx/management/commands/sync_company.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+
+from apps.asx.models import Company
+
+
+class Command(BaseCommand):
+    help = 'Sync company data with ASX official.'
+
+    def handle(self, *args, **options):
+        Company.update()
