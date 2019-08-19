c40fd12cea9d885302e8a1670e5b79fed1508627
Lorne <dev@luotao.net>
2019-08-19 10:07:22 +1000

    exlude if fiedl is null

--- apps/recommendation/strategy.py
+++ apps/recommendation/strategy.py
@@ -1,4 +1,4 @@
-from django.db.models import F
+from django.db.models import F, Q
 
 from apps.asx.models import Company
 from apps.prediction.models import WeeklyPrediction
@@ -26,7 +26,9 @@ class ReturnRiskRank(Strategy):
     def run(self, week):
         WeeklyRecommendation.objects.filter(week=week, strategy=self.name).delete()
         value_company = Company.exclude_trash(10)
-        predictions = WeeklyPrediction.objects.week(week).filter(code__in=value_company.values_list('code', flat=True))
+        predictions = WeeklyPrediction.objects.week(week).filter(
+            code__in=value_company.values_list('code', flat=True)).exclude(
+            Q(return_rank__isnull=True) | Q(risk_rank__isnull=True))
         ranked_queryset = predictions.annotate(rank=F('return_rank') - F('risk_rank')).order_by('-rank')
         recommendations = ranked_queryset[:self.count]
 
