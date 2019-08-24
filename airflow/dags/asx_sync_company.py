72a9ed2021babce1e694dbee6e7e02002c8fc236
Lorne <dev@luotao.net>
2019-08-24 18:15:06 +1000

    sync company everyday

--- airflow/dags/asx_sync_company.py
+++ airflow/dags/asx_sync_company.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 
 from airflow.operators.bash_operator import BashOperator
+from airflow.utils.dates import days_ago
 from dateutil.relativedelta import relativedelta, FR
 
 from airflow import DAG
@@ -16,7 +17,7 @@ def next_weekday(weekday):
 default_args = {
     'owner': 'luotao',
     'depends_on_past': False,
-    'start_date': next_weekday(FR),
+    'start_date': days_ago(2),
     'email': ['dev@luotao.net'],
     'email_on_failure': False,
     'email_on_retry': False,
@@ -28,7 +29,7 @@ default_args = {
     # 'end_date': datetime(2016, 1, 1),
 }
 
-dag = DAG('asx_sync_company', default_args=default_args, schedule_interval='30 18 * * 5')
+dag = DAG('asx_sync_company', default_args=default_args, schedule_interval='30 18 * * *')
 
 sync_asx_company = BashOperator(
     task_id='asx_sync_company',
