05900166e4f4ae1940662e0e17a1d7c0bb507dd2
Lorne <dev@luotao.net>
2019-09-09 12:25:16 +1000

    add airflow task for load_predict and recommend

---
+++ airflow/dags/asx_recommend.py
@@ -0,0 +1,37 @@
+from datetime import datetime, timedelta
+
+from airflow.operators.bash_operator import BashOperator
+from airflow.utils.dates import days_ago
+from dateutil.relativedelta import relativedelta, FR
+
+from airflow import DAG
+
+PYTHON = '/home/luotao/venv/hawkeye/bin/python'
+BASE_DIR = '/opt/hawkeye'
+
+
+def next_weekday(weekday):
+    return datetime.now() + relativedelta(weekday=weekday(+1))
+
+
+default_args = {
+    'owner': 'luotao',
+    'depends_on_past': False,
+    'start_date': days_ago(2),
+    'email': ['dev@luotao.net'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    # 'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+}
+
+dag = DAG('asx_recommend', default_args=default_args, schedule_interval='33 10 * * 1')
+
+asx_recommend = BashOperator(
+    task_id='asx_recommend',
+    bash_command=f'cd {BASE_DIR} && {PYTHON} manage.py recommend',
+    dag=dag)
