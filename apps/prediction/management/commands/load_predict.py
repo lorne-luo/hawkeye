5e027d108028559fa04ed02f3d6addab18527416
Lorne <dev@luotao.net>
2019-09-09 12:18:47 +1000

    update command parameter

--- apps/prediction/management/commands/load_predict.py
+++ apps/prediction/management/commands/load_predict.py
@@ -1,6 +1,8 @@
+from datetime import datetime
+
+from dateutil.relativedelta import relativedelta, FR
 from django.core.management.base import BaseCommand
 
-from apps.asx.models import Company
 from apps.prediction.models import WeeklyPrediction
 
 
@@ -8,11 +10,16 @@ class Command(BaseCommand):
     help = 'Load prediction by week.'
 
     def add_arguments(self, parser):
-        parser.add_argument('week', nargs='+', type=int)
+        parser.add_argument('week', nargs='?', type=int)
+
+    def get_last_friday(self):
+        dt = datetime.now() + relativedelta(weekday=FR(-1))
+        return dt.strftime('%Y%m%d')
 
     def handle(self, *args, **options):
-        if not options.get('week'):
-            print('Please enter week.')
+        if options.get('week'):
+            week = options['week'][0]
+        else:
+            week = self.get_last_friday()
 
-        week = options['week'][0]
         WeeklyPrediction.process_csv(week)
--- apps/recommendation/management/commands/recommend.py
+++ apps/recommendation/management/commands/recommend.py
@@ -1,7 +1,8 @@
+from datetime import datetime
+
+from dateutil.relativedelta import relativedelta, FR
 from django.core.management.base import BaseCommand
 
-from apps.asx.models import Company
-from apps.prediction.models import WeeklyPrediction
 from apps.recommendation.strategy import ReturnRiskRank
 
 
@@ -9,12 +10,17 @@ class Command(BaseCommand):
     help = 'Load prediction by week.'
 
     def add_arguments(self, parser):
-        parser.add_argument('week', nargs='+', type=int)
+        parser.add_argument('week', nargs='?', type=int)
+
+    def get_last_friday(self):
+        dt = datetime.now() + relativedelta(weekday=FR(-1))
+        return dt.strftime('%Y%m%d')
 
     def handle(self, *args, **options):
-        if not options.get('week'):
-            print('Please enter week.')
+        if options.get('week'):
+            week = options['week'][0]
+        else:
+            week = self.get_last_friday()
 
-        week = options['week'][0]
         counter = ReturnRiskRank().run(week)
         print(f'{counter} stocks recommended.')
